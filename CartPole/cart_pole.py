28d58eca5ec7b2b3cd83b9c5088247736416cade
NimishMishra <42909663+NimishMishra@users.noreply.github.com>
2018-12-25 11:31:07 +0530

    Update cart_pole.py

--- CartPole/cart_pole.py
+++ CartPole/cart_pole.py
@@ -6,11 +6,6 @@ from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
 from statistics import mean, median
 from collections import Counter
-# import tflearn
-# from tflearn.layers.core import input_data, dropout, fully_connected
-# from tflearn.layers.estimator import regression
-# from statistics import median, mean
-# from collections import Counter
 
 LR = 1e-3
 env = gym.make("CartPole-v1")
